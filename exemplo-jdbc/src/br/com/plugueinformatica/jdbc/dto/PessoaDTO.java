aaa6bec7833443bd102312882cf9b609d5736d21
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-30 00:37:02 -0300

    Classe Pessoa

---
+++ exemplo-jdbc/src/br/com/plugueinformatica/jdbc/dto/PessoaDTO.java
@@ -0,0 +1,51 @@
+package br.com.plugueinformatica.jdbc.dto;
+
+import java.util.Date;
+
+public class PessoaDTO {
+	
+	private Integer idPessoa;
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private char sexo;
+	private Date dtNascimento;
+	
+	public Integer getIdPessoa() {
+		return idPessoa;
+	}
+	public void setIdPessoa(Integer idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public Date getDtNascimento() {
+		return dtNascimento;
+	}
+	public void setDtNascimento(Date dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}		
+
+}
