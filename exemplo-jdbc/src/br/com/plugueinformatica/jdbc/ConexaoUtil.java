3a969d97cdbad9b63e6f50437a1800af28396cf2
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-30 19:37:39 -0300

    Ajustes na classe ConexaoUtil

--- exemplo-jdbc/src/br/com/plugueinformatica/jdbc/ConexaoUtil.java
+++ exemplo-jdbc/src/br/com/plugueinformatica/jdbc/ConexaoUtil.java
@@ -1,17 +1,19 @@
 package br.com.plugueinformatica.jdbc;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import br.com.plugueinformatica.jdbc.dto.PessoaDTO;
+import java.util.ResourceBundle;
 
 public class ConexaoUtil {
 
+	private static ResourceBundle config;
+
 	private static ConexaoUtil conexaoUtil;
+
+	public ConexaoUtil() {
+		config = ResourceBundle.getBundle("config");
+	}
 
 	public static ConexaoUtil getInstance() {
 		if (conexaoUtil == null) {
@@ -22,62 +24,10 @@ public class ConexaoUtil {
 
 	public Connection getConnection() throws ClassNotFoundException, SQLException {
 
-		Class.forName("com.mysql.jdbc.Driver");
-		return DriverManager.getConnection("jdbc:mysql://localhost:3306/teste_devmedia", "root", "coelhoazul12");
+		Class.forName(config.getString("br.com.plugueinformatica.bd.driver.mysql"));
+		return DriverManager.getConnection(config.getString("br.com.plugueinformatica.bd.url.conexao"),
+				config.getString("br.com.plugueinformatica.bd.usuario"), config.getString("br.com.plugueinformatica.bd.password"));
 
 	}
 	
-	public void listar() throws SQLException {
-		Connection connection = null;
-		try {
-			connection = getConnection();
-			
-			String sql = "SELECT * FROM TB_PESSOA";
-			
-			PreparedStatement statement = connection.prepareStatement(sql);
-			ResultSet resultSet = statement.executeQuery();
-			
-			while (resultSet.next()) {
-				System.out.println(resultSet.getInt("id_pessoa") + " - "
-								+ resultSet.getString("nome"));				
-				
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			connection.close();
-		}
-	}
-	
-	public void inserir(PessoaDTO pessoaDTO) throws SQLException {
-		
-	}
-	
-	public void atualizar(PessoaDTO pessoaDTO) throws SQLException {
-		
-	}
-	
-	public void remover(Integer idPessoa) throws SQLException {
-		
-	}
-	
-	public static void main(String[] args) {
-		try {			
-			PessoaDTO dto = new PessoaDTO();
-			dto.setIdPessoa(4);
-			dto.setNome("Maria Lessa de Freitas Anselmo");
-			dto.setEndereco("Rua Luiz da Silva Guerra, 13");
-			dto.setCpf("12345678945");
-			dto.setSexo('F');
-			dto.setDtNascimento(new java.util.Date());
-			
-			getInstance().remover(dto.getIdPessoa());
-			
-			getInstance().listar();
-		} catch (SQLException e) {			
-			e.printStackTrace();
-		}
-	}
-
 }
