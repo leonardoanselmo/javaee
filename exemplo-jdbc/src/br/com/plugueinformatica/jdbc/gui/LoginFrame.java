ea8711a0e664c0ae3ebcf2e310deb75a1d9593ad
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-10-31 20:34:14 -0300

    Classe da Tela de login

---
+++ exemplo-jdbc/src/br/com/plugueinformatica/jdbc/gui/LoginFrame.java
@@ -0,0 +1,26 @@
+package br.com.plugueinformatica.jdbc.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginFrame extends Application{
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent parent = FXMLLoader.
+	            load(getClass().getResource("login.fxml"));
+	        Scene scene = new Scene(parent);
+	        stage.setScene(scene);
+	        stage.setTitle("Tela de Login");
+	        stage.show();
+		
+	}
+	
+	 public static void main(String[] args) {
+	        launch(args);
+	}
+
+}
