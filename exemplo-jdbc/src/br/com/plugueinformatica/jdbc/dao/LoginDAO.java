9211d9d1070d84e7ad19ed576dcbb9728ed53bd8
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-02 21:45:56 -0300

    Classe de login para acesso aos dados no banco de dados

--- exemplo-jdbc/src/br/com/plugueinformatica/jdbc/dao/LoginDAO.java
+++ exemplo-jdbc/src/br/com/plugueinformatica/jdbc/dao/LoginDAO.java
@@ -1,15 +1,38 @@
 package br.com.plugueinformatica.jdbc.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
+import br.com.plugueinformatica.jdbc.ConexaoUtil;
 import br.com.plugueinformatica.jdbc.dto.LoginDTO;
 import br.com.plugueinformatica.jdbc.exception.PersistenciaException;
 
 public class LoginDAO implements GenericoDAO<LoginDTO>{
 	
-	public boolean logar(LoginDTO loginDTO) {
+	public boolean logar(LoginDTO loginDTO) throws PersistenciaException {
+		boolean resultado = false;
+		try {
+			Connection connection = ConexaoUtil.getInstance().getConnection();
+			
+			String sql = "SELECT * FROM TB_LOGIN WHERE NOME = ? AND SENHA = ?";
+			
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, loginDTO.getNome());
+			statement.setString(2, loginDTO.getSenha());
+			
+			ResultSet resultSet = statement.executeQuery();
+			
+			resultado = resultSet.next();			
+			connection.close();			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new PersistenciaException(e.getMessage(), e);
+		}
 		
-		return false;
+		return resultado;
 	}
 
 	@Override
