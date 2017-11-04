bd9c731b110087cb1ac3cef692e37043e0a7953b
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-04 12:06:45 -0300

    Classe controller de login para acesso ao sistema

--- exemplo-jdbc/src/br/com/plugueinformatica/jdbc/controller/LoginController.java
+++ exemplo-jdbc/src/br/com/plugueinformatica/jdbc/controller/LoginController.java
@@ -3,83 +3,79 @@ package br.com.plugueinformatica.jdbc.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javax.swing.JOptionPane;
-
 import br.com.plugueinformatica.jdbc.bo.LoginBO;
 import br.com.plugueinformatica.jdbc.dto.LoginDTO;
 import br.com.plugueinformatica.jdbc.exception.NegocioException;
-import br.com.plugueinformatica.jdbc.util.MensagensUtil;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController implements Initializable {
 	
+
 	@FXML
-	private TextField txtLogin = new TextField(); 
-	@FXML
-	private PasswordField txtSenha = new PasswordField();
-	@FXML
-	private Button btnEntrar, btnSair;
-	
+    private TextField txtLogin;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private Button btnSair;
+
+    @FXML
+    private Button btnEntrar;	
 	
 	@Override
-	public void initialize(URL url, ResourceBundle resources) {		
-		btnEntrar.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				//System.exit(0);
-				btnEntrarAction();
-			}
-		});
-		
-		btnSair.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				System.exit(0);
-			}
-		});
+	public void initialize(URL location, ResourceBundle resources) {
 		
 		
 	}
 	
-	public void mostrarMensagem() {
-		if (txtLogin.getText().equals("admin")){
-			JOptionPane.showMessageDialog(null, "ok");
-		}
-	}
-	
-	
-	//@FXML
-	//public void btnEntrarAction(ActionEvent evt) {
-	public void btnEntrarAction() {
+	@FXML
+    void entrarNoSistema() {
 		LoginDTO loginDTO = new LoginDTO();
-				
+		
 		loginDTO.setNome(txtLogin.getText());
 		loginDTO.setSenha(txtSenha.getText());
 		LoginBO loginBO = new LoginBO();
 		try {
 			if(loginBO.logar(loginDTO)) {
-				MensagensUtil.addMsg(LoginController.this, "Login efetuado com sucesso.");
+				exibirDialogoInformacao("Login efetuado com sucesso.");
 			} else {
-				MensagensUtil.addMsg(LoginController.this, "Dados inválidos.");
+				exibirDialogoErro("Dados inválidos.");
 			}
 		} catch (NegocioException e) {		
 			e.printStackTrace();
-			MensagensUtil.addMsg(LoginController.this, e.getMessage());
+			exibirDialogoErro(e.getMessage());
 		}
+
+    }
+
+    @FXML
+    void sairDoSistema() {
+    		System.exit(0);
+    }    
+    
+    private void exibirDialogoInformacao(String informacao) {
+    		Alert alert = new Alert(AlertType.INFORMATION);
+    		alert.setTitle("Informação");
+    		alert.setHeaderText(null);
+    		alert.setContentText(informacao);
+    		
+    		alert.showAndWait();
+    }
+    
+    private void exibirDialogoErro(String erro) {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Informação");
+		alert.setHeaderText(null);
+		alert.setContentText(erro);
 		
-	}
-	
-	/*
-	@FXML
-	protected void btnSairAction(ActionEvent evt) {		
-		System.exit(0);
-	}
-	*/
+		alert.showAndWait();
+    }	
 
 }
