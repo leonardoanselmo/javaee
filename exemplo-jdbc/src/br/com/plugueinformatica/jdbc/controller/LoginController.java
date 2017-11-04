3638a8ee7ab8ce8f5d53810002e0a44043f45a03
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-03 21:27:54 -0300

    Classe controller de login

--- exemplo-jdbc/src/br/com/plugueinformatica/jdbc/controller/LoginController.java
+++ exemplo-jdbc/src/br/com/plugueinformatica/jdbc/controller/LoginController.java
@@ -3,32 +3,62 @@ package br.com.plugueinformatica.jdbc.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.JOptionPane;
+
 import br.com.plugueinformatica.jdbc.bo.LoginBO;
 import br.com.plugueinformatica.jdbc.dto.LoginDTO;
 import br.com.plugueinformatica.jdbc.exception.NegocioException;
 import br.com.plugueinformatica.jdbc.util.MensagensUtil;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController implements Initializable {
 	
 	@FXML
-	private TextField txtLogin;
+	private TextField txtLogin = new TextField(); 
 	@FXML
-	private PasswordField txtSenha;
+	private PasswordField txtSenha = new PasswordField();
+	@FXML
+	private Button btnEntrar, btnSair;
 	
 	
 	@Override
-	public void initialize(URL url, ResourceBundle resources) {
-					
+	public void initialize(URL url, ResourceBundle resources) {		
+		btnEntrar.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				//System.exit(0);
+				btnEntrarAction();
+			}
+		});
+		
+		btnSair.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.exit(0);
+			}
+		});
+		
+		
 	}
 	
-	@FXML
-	protected void btnEntrarAction(ActionEvent evt) {
+	public void mostrarMensagem() {
+		if (txtLogin.getText().equals("admin")){
+			JOptionPane.showMessageDialog(null, "ok");
+		}
+	}
+	
+	
+	//@FXML
+	//public void btnEntrarAction(ActionEvent evt) {
+	public void btnEntrarAction() {
 		LoginDTO loginDTO = new LoginDTO();
+				
 		loginDTO.setNome(txtLogin.getText());
 		loginDTO.setSenha(txtSenha.getText());
 		LoginBO loginBO = new LoginBO();
@@ -42,11 +72,14 @@ public class LoginController implements Initializable {
 			e.printStackTrace();
 			MensagensUtil.addMsg(LoginController.this, e.getMessage());
 		}
+		
 	}
 	
+	/*
 	@FXML
-	protected void btnSairAction(ActionEvent evt) {
+	protected void btnSairAction(ActionEvent evt) {		
 		System.exit(0);
 	}
+	*/
 
 }
