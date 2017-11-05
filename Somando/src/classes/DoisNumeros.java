7a141cb9ef5e0a05a65ce560f68ee50f0f0bb717
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-05 15:45:31 -0300

    Exemplo java Somando dois numeros

---
+++ Somando/src/classes/DoisNumeros.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class DoisNumeros {
+	
+	public double somar(double n1, double n2) {
+		return n1+n2;
+	}
+
+}
---
+++ Somando/src/principal/Aplicacao.java
@@ -0,0 +1,15 @@
+package principal;
+
+import classes.DoisNumeros;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		DoisNumeros doisNumeros = new DoisNumeros();
+		double result = doisNumeros.somar(2,2);
+		System.out.println(result);
+
+	}
+
+}
