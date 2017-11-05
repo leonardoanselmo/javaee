4186dd1d50dd13529b07e21085f64fc88263f814
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-05 10:50:05 -0300

    Classe de negocios de Pessoa

---
+++ exemplo-jdbc/src/br/com/plugueinformatica/jdbc/bo/PessoaBO.java
@@ -0,0 +1,14 @@
+package br.com.plugueinformatica.jdbc.bo;
+
+import br.com.plugueinformatica.jdbc.dto.PessoaDTO;
+import br.com.plugueinformatica.jdbc.util.MensagensUtil;
+
+public class PessoaBO {
+	
+	public void cadastrar(PessoaDTO pessoaDTO) {
+		
+	}
+	
+	
+
+}
