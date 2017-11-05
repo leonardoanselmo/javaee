5b6006898ebab0400673cc5fe005e5d8179a6113
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-05 16:15:58 -0300

    Classe de negocio de Pessoa

--- exemplo-jdbc/src/br/com/plugueinformatica/jdbc/bo/PessoaBO.java
+++ exemplo-jdbc/src/br/com/plugueinformatica/jdbc/bo/PessoaBO.java
@@ -1,11 +1,21 @@
 package br.com.plugueinformatica.jdbc.bo;
 
+import br.com.plugueinformatica.jdbc.dao.PessoaDAO;
 import br.com.plugueinformatica.jdbc.dto.PessoaDTO;
-import br.com.plugueinformatica.jdbc.util.MensagensUtil;
+import br.com.plugueinformatica.jdbc.exception.NegocioException;
+
 
 public class PessoaBO {
 	
-	public void cadastrar(PessoaDTO pessoaDTO) {
+	public void cadastrar(PessoaDTO pessoaDTO) throws NegocioException {
+		
+		try {
+			PessoaDAO pessoaDAO = new PessoaDAO();
+			pessoaDAO.inserir(pessoaDTO);			
+		} catch (Exception exception) {
+			throw new NegocioException(exception.getMessage()); 			
+		}
+		
 		
 	}
 	
