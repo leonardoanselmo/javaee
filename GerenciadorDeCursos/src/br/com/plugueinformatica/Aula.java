73de6213bf40958c26b03f7d0469a28c1628a161
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-10 20:30:00 -0300

    Classe Aula

---
+++ GerenciadorDeCursos/src/br/com/plugueinformatica/Aula.java
@@ -0,0 +1,23 @@
+package br.com.plugueinformatica;
+
+public class Aula {
+	
+	private String nome;
+	private Integer tempo;
+	
+	public Aula(String nome, Integer tempo) {		
+		this.nome = nome;
+		this.tempo = tempo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public Integer getTempo() {
+		return tempo;
+	}
+	
+	
+
+}
