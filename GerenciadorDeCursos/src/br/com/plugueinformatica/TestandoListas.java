dc81cbc522511ae22966ffab996f6d2b1e6dceaf
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-10 23:21:10 -0300

    Classe TestandoListas

--- GerenciadorDeCursos/src/br/com/plugueinformatica/TestandoListas.java
+++ GerenciadorDeCursos/src/br/com/plugueinformatica/TestandoListas.java
@@ -1,10 +1,42 @@
 package br.com.plugueinformatica;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class TestandoListas {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Leonardo");
+		String aula1 = "Conhecendo mais de listas";
+		String aula2 = "Modelando a classe aula";
+		String aula3 = "Trabalhando com Cursos e sets";
+
+		ArrayList<String> aulas = new ArrayList<>();
+
+		aulas.add(aula1);
+		aulas.add(aula2);
+		aulas.add(aula3);
+
+		System.out.println(aulas);
+		aulas.remove(0);
+		System.out.println(aulas);
+
+		for (String aula :aulas) {
+			System.out.println("Aula: "+aula);
+		}
+
+		System.out.println(aulas.size());
+
+		aulas.forEach(aula -> {
+			System.out.println("Percorrendo: ");
+			System.out.println("Aula: "+aula);
+		});
+
+		aulas.add("Aumentando nosso conhecimento");
+		Collections.sort(aulas);
+
+		System.out.println(aulas);
 
 	}
 
