f714d35d70cbad52e8e8d0d1a6d6d4483e1f9a11
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-21 15:27:04 -0300

    Exemplo de um CRUD com JPA2 e Hibernate

---
+++ crud-jpa2-hibernate/src/br/com/plugueinformatica/SalvandoPrimeiroObjeto.java
@@ -0,0 +1,35 @@
+package br.com.plugueinformatica;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.plugueinformatica.model.Cliente;
+
+public class SalvandoPrimeiroObjeto {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemploPU");
+		EntityManager em = emf.createEntityManager();
+		
+		Cliente cliente = new Cliente();
+		cliente.setNome("Leonardo Anselmo");
+		cliente.setIdade(38);
+		cliente.setProfissao("Programador");
+		cliente.setSexo("M");
+		
+		em.getTransaction().begin();		
+		em.persist(cliente);
+		em.getTransaction().commit();
+		
+		
+		System.out.println("Cliente salvo com sucesso!");
+		
+		
+		//em.close();
+		
+
+	}
+
+}
