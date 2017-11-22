0188f2dc7302b5a1aed76781d940dda5cd2d6f77
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-11-21 22:35:42 -0300

    Classe InserindoContatos com JPA2 e Hibernate

--- AgendaTelefonica/src/br/com/pluginformatica/main/InserindoContatos.java
+++ AgendaTelefonica/src/br/com/pluginformatica/main/InserindoContatos.java
@@ -1,11 +1,27 @@
 package br.com.pluginformatica.main;
 
+import java.sql.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.pluginformatica.model.Agenda;
+
 public class InserindoContatos {
 
 	public static void main(String[] args) {
 		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("dbAgenda");
+		EntityManager em = emf.createEntityManager();
 		
+		Agenda contato = new Agenda("Urso Anselmo", "(87)9.9988-0001", Date.valueOf("2017-11-21"));
 		
+		em.getTransaction().begin();
+		em.persist(contato);
+		em.getTransaction().commit();
+		
+		System.out.println("Contato salvo com sucesso!");		
 
 	}
 
